3ea2ebcf64dd2b15524158dcf3fb21f99d760604
Nizijolas <nicolasdijoud@yahoo.com>
2026-03-04 20:15:02 +0100

    o

--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { Store } from '@ngrx/store';
 import { loadNextPoem } from '../store/poems.actions';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-side-nav',
@@ -11,6 +12,8 @@ import { loadNextPoem } from '../store/poems.actions';
   styleUrls: ['./side-nav.component.css']
 })
 export class SideNavComponent implements OnInit{
+
+  @ViewChild('drawer') drawer!:MatSidenav;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -26,6 +29,7 @@ export class SideNavComponent implements OnInit{
    currentNav: string = "home";
 
   changeCurrentNav(newNav: string) {
+    this.drawer.toggle();
     this.currentNav = newNav;
   }
 
